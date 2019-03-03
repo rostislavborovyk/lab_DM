5342404b779fb5606fae0d3a8fe48aa519e263fc
rostmoguchiy@gmail.com <3831200011nnn>
2019-03-03 13:07:47 +0200

    New version 4

--- func.py
+++ func.py
@@ -11,3 +11,8 @@ def universal(u):
 def str_to_set(stri):
     A={int(i) for i in stri if i.isdecimal()}
     return A
+
+def print_label(label_list, entry_list):
+    label_list[0].configure(text=str(entry_list[0].get()))
+    label_list[1].configure(text=str(entry_list[1].get()))
+    label_list[2].configure(text=str(entry_list[2].get()))
