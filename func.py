44f466a8f0d7846524e678a7fc3be0dbb1a84f74
rostmoguchiy@gmail.com <3831200011nnn>
2019-03-03 15:08:26 +0200

    New version 14

--- func.py
+++ func.py
@@ -11,7 +11,6 @@ def rand(a):
     return A
 
 
-
 def universal(u):
     U = {i for i in range(1,u)}
     return U
@@ -24,19 +23,21 @@ def str_to_set(stri):
     # 1,2,3,4,5 --> {1,2,3,4,5}
     elements = stri.split(',')
     for i in elements:
-        if i.isdecimal():
-            sett.add(int(i))
-
+        sett.add(int(i))
     return sett
+
 
 def print_label(label_list, entry_list):
     global A,B,C
     label_list[0].configure(text=str(rand(entry_list[0].get())))
     label_list[1].configure(text=str(rand(entry_list[1].get())))
     label_list[2].configure(text=str(rand(entry_list[2].get())))
+    print('A is '+str(str_to_set(label_list[0].cget("text"))))
     A=(str_to_set(label_list[0].cget("text")))
     B=(str_to_set(label_list[1].cget("text")))
     C=(str_to_set(label_list[2].cget("text")))
+
+
 def print_manlabel(label_list, entry_list):
     global A,B,C
     label_list[0].configure(text=str(str_to_set(entry_list[0].get())))
@@ -49,7 +50,7 @@ def print_manlabel(label_list, entry_list):
 def u_print(l,l1):
     global U
     l.configure(text=str(universal(int(l1.get()))))
-    U=str_to_set(l.cget("text"))
+    U = str_to_set(l.cget("text"))
 
 
 def print_from_constants(label_list):
@@ -57,6 +58,5 @@ def print_from_constants(label_list):
     label_list[0].configure(text=str(A))
     label_list[1].configure(text=str(B))
     label_list[2].configure(text=str(C))
-
 
 print(str_to_set('{1,2,3,4,5}'))
